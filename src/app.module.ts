ffac655fa1d2cc166776c853d88f93c26c944be0
rafael mendes <rafael.mendes@tallos.com.br>
2022-06-29 16:49:05 -0300

    adicionando arquivos e dependecias de auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FuncionariosModule } from './Funcionarios/funcionarios.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://root:root@localhost:27017/db-funcionarios?authSource=admin'), FuncionariosModule],
+  imports: [MongooseModule.forRoot('mongodb://root:root@localhost:27017/db-funcionarios?authSource=admin'), FuncionariosModule, AuthModule],
   controllers: [],
   providers: [],
 })
